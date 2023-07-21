a57ff32329f2d96b59726b6a16c2ecb339ee5045
Eugene <y.zorych@tapston.com>
2023-07-21 14:36:03 +0300

    init styles

---
+++ source/styles/index.ts
@@ -0,0 +1,25 @@
+import {Dimensions} from 'react-native';
+
+const {height, width} = Dimensions.get('window');
+
+export const screenHeight = height;
+export const screenWidth = width;
+
+export const colors = {
+  blue: '#0C48A1',
+  lightBlue: '#E6EEF8',
+  halfBlack: '#637A86',
+  middleGray: '#E9EDEF',
+  black: '#232B2F',
+  blueGray: '#BBC5CB',
+  lightGray: '#F7F7F7',
+  green: '#27C07D',
+  lightGreen: '#E9F9F2',
+};
+
+export const hitClop = {
+  top: 12,
+  bottom: 12,
+  left: 12,
+  right: 12,
+};
