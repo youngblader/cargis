5a06a51187267acf50ba0211239b3ba7e43b430c
Eugene <y.zorych@tapston.com>
2023-07-24 13:06:38 +0300

    add mock categories

---
+++ source/config/index.ts
@@ -0,0 +1,20 @@
+import {Category} from '../types/Category';
+
+export const categories: Category[] = [
+  {
+    id: 0,
+    name: 'Все',
+  },
+  {
+    id: 1,
+    name: 'Активные',
+  },
+  {
+    id: 2,
+    name: 'На паузе',
+  },
+  {
+    id: 3,
+    name: 'Завершенные',
+  },
+];
